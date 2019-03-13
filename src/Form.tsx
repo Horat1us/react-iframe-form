71e9ee9b549f1f95968c9bd8e1953e2575893966
Alexander Letnikow <reclamme@gmail.com>
2019-03-13 02:35:15 +0200

    Implement iframe form

---
+++ src/Form.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Config } from "./config";
+
+export interface FormProps {
+    name: string;
+    config: Config,
+}
+
+export const Form: React.FunctionComponent<FormProps> = React.memo(({ config, name }) => {
+    const [ isSent, setIsSent ] = React.useState<boolean>(false);
+
+    const submitOnMount = React.useCallback((form?: HTMLFormElement) => {
+        if (!form) {
+            return;
+        }
+
+        form.submit();
+        setIsSent(true);
+    }, [ setIsSent ]);
+
+    return (
+        <>
+            <iframe name={name}/>
+            {!isSent && (
+                <form target={name} action={config.url} method={config.method} ref={submitOnMount}>
+                    {Object.keys(config.data).map((key) => (
+                        <input
+                            type={"hidden"}
+                            name={key}
+                            value={
+                                ("string" === typeof config.data[ key ])
+                                    ? config.data[ key ]
+                                    : JSON.stringify(config.data[ key ])
+                            }
+                        />
+                    ))}
+                </form>
+            )}
+        </>
+    )
+});
+
+Form.displayName = "IFrameForm";
