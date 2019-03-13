e307ce23a9895789a1340e8daa26e1acc87bb0b7
Alexander Letnikow <reclamme@gmail.com>
2019-03-13 02:35:28 +0200

    Implement async iframe form

---
+++ src/AsyncForm.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { Form } from "./Form";
+import { Config } from "./Config";
+
+export interface AsyncFormProps {
+    name: string;
+    fetchConfig: () => Promise<Config>;
+}
+
+export const AsyncForm: React.FunctionComponent<AsyncFormProps> = React.memo(({ fetchConfig, name }) => {
+    const [ config, setConfig ] = React.useState<Config | undefined>();
+
+    React.useEffect(() => {
+        fetchConfig().then(setConfig);
+    });
+
+    if (!config) {
+        return null;
+    }
+
+    return <Form name={name} config={config}/>;
+});
+
+AsyncForm.displayName = "AsyncIFrameForm";
